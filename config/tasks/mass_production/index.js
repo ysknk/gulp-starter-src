19249513886c7529cf9037a5c1f597f038730394
ysknk <31568245+ysknk@users.noreply.github.com>
2020-11-17 18:11:31 +0900

    Update index.js

--- config/tasks/mass_production/index.js
+++ config/tasks/mass_production/index.js
@@ -7,14 +7,14 @@ const plugin = require('../../../../_app/gulpfile.babel.js/plugins/pug');
 /**
  * Set Const Variables
  */
-const config = global[define.ns];
+// const config = global[define.ns];
 
 /**
  * Set Variables
  */
 const task = {
   name: 'mass_production',
-  types: []// **:watch function [0] || 'proc'
+  types: ['build']// **:watch function [0] || 'proc'
 };
 
 /**
@@ -52,43 +52,144 @@ class MassProduction extends TaskMaster {
    * @param {object} stream gulp object
    * @param {function} done set complete
    */
-  procedure(stream, done) {
-    const items = require(path.resolve(this.task.data.itemsfile));
+  build(stream, done) {
+    const contexts = require(path.resolve(this.task.data.contextDir));
 
-    for (let item of items) {
-      const extname = (item.extname || this.task.data.extension)
-      const destDir = (item.destDir
-        ? item.destDir + '/'
-        : ''
-      );
+    for (const key in contexts) {
 
-      const dest = item.dest
-        ? item.dest + destDir
-        : this.task.data.dest + destDir;
+      const context = contexts[key];
+      const {dest, extname} = this.getDest(context);
+      const template = path.resolve(`${this.task.data.templateDir}${key}.pug`);
 
-      stream
-        .pipe($.rename(item.filename + extname))
-        .pipe($.data((file) => {
-          item.data = _.merge({},
-            this.task.data.meta,
-            item.data
-          );
-          item.data.root_path = this.task.data.root_path;
-          if (item.destDir) {
-            console.log(`${dest}${item.filename + extname}`);
-          }
-          return item.data;
-        }))
+      switch (key) {
+        case 'tests': {
+          context.forEach((parent) => {
+            const parentSlug = parent.slug || '';
+            const {filename, slug} = this.getPath(parentSlug)
 
-        .pipe(plugin(this.task.data.options))
-        .pipe(plugins.useful(this.task.data.convert))
-        .pipe(gulp.dest(dest))
+            this.createPage({
+              dest: `${dest}${slug}`,
+              filename: `${filename}${extname}`,
+              template,
+              context: parent,
+            });
 
-        .pipe($.size(this.sizeOptions()))
-        .pipe(plugins.log())
+            parent.list.forEach((child) => {
+              const childSlug = child.slug;
+              const pageslug = `${parentSlug}${childSlug}`;
 
-        .on('finish', () => {done && done();});
+              const {filename, slug} = this.getPath(pageslug);
+
+              this.createPage({
+                dest: `${dest}${slug}`,
+                filename: `${filename}${extname}`,
+                template,
+                context: child
+              });
+              // console.log(`Create: ${dest}${slug}index${extname}`);
+            });
+          });
+          break;
+        }
+
+        default: {
+          context.forEach((page) => {
+            const {filename, slug} = this.getPath(page.slug)
+
+            this.createPage({
+              dest: `${dest}${slug}`,
+              filename: `${filename}${extname}`,
+              template,
+              context: page
+            });
+          });
+          break;
+        }
+      }
+
+      done && done()
     }
+  }
+
+  /**
+   * getPath
+   *
+   * @param {string} slug
+   * @returns {object}
+   */
+  getPath(slug) {
+    const lastdir = slug.match(/\/(.*)$/);
+    const lastslash = slug.match(/\/$/);
+
+    let filename = `index`;
+
+    if (!lastslash) {
+      if (lastdir) {
+        filename = lastdir[1];
+        slug = slug.replace(lastdir[1], '');
+      } else {
+        filename = slug;
+        slug = '';
+      }
+    }
+
+    return {
+      filename,
+      slug
+    }
+  }
+
+  /**
+   * createPage
+   *
+   * @param {object}
+   */
+  createPage({dest, filename, template, context}) {
+    const context_data = {
+      data: context
+    };
+
+    gulp.src(template, {allowEmpty: true})
+      .pipe($.rename(filename))
+      .pipe($.data(() => {
+        return this.setData(context_data)
+      }))
+
+      .pipe(plugin(this.task.data.options))
+      .pipe(plugins.useful(this.task.data.convert))
+      .pipe(gulp.dest(dest))
+
+      .pipe($.size(this.sizeOptions()))
+      .pipe(plugins.log())
+  }
+
+  /**
+   * getDest
+   *
+   * @param {object}
+   * @returns {object}
+   */
+  getDest({destDir, extname, dest}) {
+    destDir = (destDir ? destDir + '/' : '');
+    return {
+      extname: (extname || this.task.data.extension),
+      dest: dest ? (dest + destDir) : (this.task.data.dest + destDir)
+    };
+  }
+
+  /**
+   * setData
+   *
+   * @param {object} context
+   * @returns {object}
+   */
+  setData(context) {
+    context.data = _.merge({},
+      this.task.data.meta,
+      context.data
+    );
+    context.data.root_path = this.task.data.root_path;
+    return context.data;
   }
 
   /**
@@ -98,10 +199,24 @@ class MassProduction extends TaskMaster {
     let defaultTask = this.task.types && this.task.types.length ?
       this.task.types[0] : 'procedure';
     let src = this.task.data.src;
+    let mergeSrc = [...src];
 
     // default task
     gulp.task(this.task.name, (done) => {
       this[defaultTask](gulp.src(src, {allowEmpty: true}), done);
+    });
+
+    // watch task
+    gulp.task(this.task.name + ':watch', () => {
+      this.watch(this.task, mergeSrc);
+    });
+
+    // other types task
+    _.forEach(this.task.types, (type) => {
+      if(!this[type]) return;
+      gulp.task(this.task.name + ':' + type, (done) => {
+        this[type](gulp.src(src, {allowEmpty: true}), done);
+      });
     });
   }
 
