9ae717755b2af7a70fb4263cf1354fc3453ae20a
ysknk <31568245+ysknk@users.noreply.github.com>
2020-11-20 14:15:28 +0900

    Update top.js

--- htdocs/js/pages/top.js
+++ htdocs/js/pages/top.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const FN = win[NS];
-  const PAGE_NAME = `${PREFIX}top-page`;
+  const PAGE_NAME = `${meta.p.l}top-page`;
 
   function isCurrentPage() {
     return doc.querySelector(`.${PAGE_NAME}`) || ``
