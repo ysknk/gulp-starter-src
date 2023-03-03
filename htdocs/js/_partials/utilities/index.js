0b0ea814c0d254d9897f257f979a19f138ba4df9
ysknk <31568245+ysknk@users.noreply.github.com>
2023-03-03 21:38:44 +0900

    Update index.js

--- htdocs/js/_partials/utilities/index.js
+++ htdocs/js/_partials/utilities/index.js
@@ -401,3 +401,25 @@ export const debounce = (func, interval) => {
   }
 }
 
+/**
+ * filter
+ *
+ * @param {array} array
+ * @param {predicate} function
+ * @returns {array}
+*/
+export const filter = (array, predicate) => {
+  let index = -1
+  let resIndex = 0
+  const length = array == null ? 0 : array.length
+  const result = []
+
+  while (++index < length) {
+    const value = array[index]
+    if (predicate(value, index, array)) {
+      result[resIndex++] = value
+    }
+  }
+  return result
+}
+
