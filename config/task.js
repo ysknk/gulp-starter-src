4b2fcb9b5f4857f1564f25a564d631a64c18b8f6
ysknk <31568245+ysknk@users.noreply.github.com>
2024-08-06 18:11:34 +0900

    Update task.js

--- config/task.js
+++ config/task.js
@@ -178,6 +178,7 @@ module.exports = {
     //   plugins: [
     //     new webpack.DefinePlugin({
     //       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    //       'process.env.PRJ_ENV': JSON.stringify(process.env.PRJ_ENV),
     //       meta: {
     //         p: JSON.stringify(meta.p)
     //       }
